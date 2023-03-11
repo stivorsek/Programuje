cf61d09806de3e6e3277cb267ca313b5cd81bb36
Sigmorv2 <sigmorek@gmail.com>
2023-03-11 01:51:13 +0100

    Zadanie dzień 3

--- Wyzwanie21dni/Wyzwanie21dni/Program.cs
+++ Wyzwanie21dni/Wyzwanie21dni/Program.cs
@@ -1,2 +1,114 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var number1 = 11;
+var number2 = 10;
+
+// ==
+// !=
+// <
+// >
+// <=
+// >=
+
+var name = "Damian";
+var plec = "Mężczyzna";
+var age = 15;
+
+bool isActive = true;
+
+if (plec == "Kobieta" && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+
+else if (name == "Ewa")
+{
+    Console.WriteLine(name);
+    if (age == 33)
+    {
+        Console.WriteLine(", lat " + age);
+    }
+
+}
+else if (age < 18)
+{
+    Console.Write("Niepłnoletni");
+    if (!(plec == "Kobieta"))
+    {
+        Console.Write(" Mężczyzna");
+    }
+}
+
+// &&
+// ||
+// !
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//liczby calkowite
+
+//int myAge2 = 120;
+//int myAge3 = myAge2 + 5;
+//Console.WriteLine(myAge3);
+//int myVar = -5;
+//uint myVar2 = uint.MaxValue;
+
+// liczby zmiennoprzecinkowe\
+//float myNumber = float.MaxValue;
+//double myVar3 = double.MaxValue;
+
+// zmienne tekstowe
+//string name = "Damian";
+//string surname = "Dzieciol";
+//string results = name + " " + surname + " "+myAge2;
+//Console.WriteLine(results);
+//char myVar4 = 'a';
+//var result2 = name.ToArray();
+
+// zmienna logiczna
+//bool isActive = false;
+//isActive = true;
+//var isValid = 5 > 6;
+//Console.WriteLine(isValid);
